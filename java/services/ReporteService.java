770547f869fbe6dfb73534178424cd7f13c75419
Valerytintaya <distol303@gmail.com>
2025-09-05 10:16:20 -0500

    Update ReporteService.java

--- java/services/ReporteService.java
+++ java/services/ReporteService.java
@@ -7,14 +7,8 @@ import model.Reserva;
 
 public class ReporteService {
 
-    public double ocupacion(List<Habitacion> habitaciones, List<Reserva> reservas, LocalDate desde, LocalDate hasta) {
-        long total = habitaciones.size();
-        long ocupadas = reservas.stream()
-                .filter(r -> !r.isCancelada() && r.getDesde().isBefore(hasta) && r.getHasta().isAfter(desde))
-                .map(r -> r.getHabitacion().getId())
-                .distinct().count();
-        return (ocupadas * 100.0) / total;
-    }
+    
 }
+
 
 
